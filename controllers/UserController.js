0ffc5ce61f68d496a48f526aa08ff5ddb5200861
Deimer Ramos Cuello <dramos.cuello@gmail.com>
2017-03-17 23:25:38 -0500

    inserta registros

---
+++ controllers/UserController.js
@@ -0,0 +1,34 @@
+var mysql = require('mysql');
+var bcrypt = require('bcryptjs');
+module.exports = {
+  getSignUp : function(req, res, next){
+    return res.render('signup');
+  },
+
+
+  postSignUp : function(req, res, next){
+    var salt = bcrypt.genSaltSync(10);
+    var password = bcrypt.hashSync(req.body.password, salt);
+
+		var user = {
+			email : req.body.email,
+			nombre : req.body.nombre,
+			password : password
+		};
+
+    var config = require('.././database/config');
+
+    var db = mysql.createConnection(config);
+
+		db.connect();
+
+		db.query('INSERT INTO users SET ?', user, function(err, rows, fields){
+			if(err) throw err;
+
+			db.end();
+		});
+    //req.flash('info', 'Se ha registrado correctamente, ya puede iniciar sesion');
+    //return res.redirect('/auth/signin');
+    return;
+  }
+};
--- routes/routes.js
+++ routes/routes.js
@@ -3,5 +3,7 @@ var router = express.Router();
 var controllers = require('.././controllers');
 
 router.get('/', controllers.HomeController.index);
+router.get('/signup', controllers.UserController.getSignUp);
+router.post('/signup', controllers.UserController.postSignUp);
 
 module.exports = router;
