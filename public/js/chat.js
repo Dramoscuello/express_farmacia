b566cdd2456420d6a225a3359b79833dfb847755
Deimer Ramos Cuello <dramos.cuello@gmail.com>
2017-03-22 14:17:08 -0500

    added icon cart

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,17 +1,12 @@
-(function() {
+$( document ).ready(function() {
+    $('.chat').slideToggle(0, 'swing');
 
     $('#live-chat header').on('click', function() {
-
         $('.chat').slideToggle(300, 'swing');
-        $('.chat-message-counter').fadeToggle(300, 'swing');
-
     });
 
     $('.chat-close').on('click', function(e) {
-
         e.preventDefault();
         $('#live-chat').fadeOut(300);
-
     });
-
-}) ();
+});
